70a060c41ab1f2ce9da73dc554fa2f9565f39f2b
IhebChatti <iheb.chatti@holbertonschool.com>
2020-12-16 13:19:21 +0100

    inser sorted sll

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -15,17 +15,16 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *new;
 	listint_t *current;
 
+	current = *head;
 	new = malloc(sizeof(listint_t));
-	if (!new || !head)
+	if (!new)
 		return (NULL);
 	new->n = number;
-	new->next = NULL;
 	if (!*head || (*head)->n >= new->n)
 	{
 		new->next = *head;
 		*head = new;
 	}
-	current = *head;
 	while (current->next && current->next->n < new->n)
 		current = current->next;
 	new->next = current->next;
