9f2309da3a9d57a73c0fd0b976ecae6602739e02
IhebChatti <iheb.chatti@holbertonschool.com>
2020-12-18 09:42:52 +0000

    fix new->next

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -14,6 +14,7 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new;
 	listint_t *current;
+	listint_t *temp;
 
 	current = *head;
 	new = malloc(sizeof(listint_t));
@@ -27,7 +28,8 @@ listint_t *insert_node(listint_t **head, int number)
 	}
 	while (current->next && current->next->n < new->n)
 		current = current->next;
-	new->next = current->next;
+	temp = current->next;
 	current->next = new;
+	new->next = temp;
 	return (new);
 }
