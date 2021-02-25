920aa1af593d53177569512967c18748353453de
IhebChatti <iheb.chatti@holbertonschool.com>
2021-02-25 17:19:55 +0100

    slide line

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,51 @@
+#include "slide_line.h"
+
+/**
+ * slide_line - function that slides and merges an array of integers
+ * @line: points to an array of integers
+ * @size: size of line
+ * @direction: either left or right
+ *
+ * Return: 1 upon success, or 0 upon failure
+ */
+int slide_line(int *line, size_t size, int direction)
+{
+	int *current, *right, *left;
+
+	current = right = left = NULL;
+	if (direction == SLIDE_LEFT)
+	{
+		current = line;
+		left = current;
+
+		while (left < line + (size - 1))
+		{
+			while (*left == 0 && left < line + (size - 1))
+				left++;
+			right = left + 1;
+			while (right < line + (size))
+			{
+				if (*right != *left)
+					right++;
+				else
+				{
+					*current = *left * 2;
+					if (current != left)
+					{
+						*left = 0;
+					}
+					*right = 0;
+					current++;
+					break;
+				}
+			}
+			left++;
+		}
+		if (!*current && *(line + size - 1))
+		{
+			*current = *(line + size - 1);
+			*(line + size - 1) = 0;
+		}
+	}
+	return (1);
+}
---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,16 @@
+#ifndef __SLIDELINE__
+#define __SLIDELINE__
+
+/* macros */
+#define SLIDE_LEFT 0
+#define SLIDE_RIGHT 1
+
+/* Includes */
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/* Functions prototypes */
+int slide_line(int *line, size_t size, int direction);
+
+#endif
