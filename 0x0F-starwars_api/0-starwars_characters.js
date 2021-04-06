c628b548b9fc95b957131188f9b52faff1c9cdcf
IhebChatti <iheb.chatti@holbertonschool.com>
2021-04-06 20:52:34 +0100

    fix await with new promise

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,14 +2,17 @@
 const request = require('request');
 const filmId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
-request(url, (error, response, body) => {
+request(url, async (error, response, body) => {
   if (!error) {
     const characters = JSON.parse(body).characters;
     for (const charId in characters) {
-      request(characters[charId], (error, response, body) => {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
+      await new Promise((resolve, reject) => {
+        request(characters[charId], (error, response, body) => {
+          if (!error) {
+            console.log(JSON.parse(body).name);
+            resolve();
+          }
+        });
       });
     }
   }
