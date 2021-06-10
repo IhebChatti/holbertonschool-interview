85240876bbf05885b66d23ae57275ab5794f932d
IhebChatti <iheb.chatti@holbertonschool.com>
2021-06-10 22:00:47 +0100

    merge sort

---
+++ 0x18-merge_sort/sort.h
@@ -0,0 +1,12 @@
+#ifndef SORT_H
+#define SORT_H
+
+/* includes */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* functions prototypes */
+void print_array(const int *array, size_t size);
+void merge_sort(int *array, size_t size);
+
+#endif
