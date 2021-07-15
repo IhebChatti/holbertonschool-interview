c60dfb053a8d49865450216010481edff2827da5
IhebChatti <iheb.chatti@holbertonschool.com>
2021-07-15 20:57:05 +0100

    docstring and pep8

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -1,4 +1,12 @@
 def island_perimeter(grid):
+    """[island_perimeter]
+
+    Args:
+        grid ([list of list of integers]): [island]
+
+    Returns:
+        [integer]: [perimeter of the island]
+    """
     area = 0
     for row in grid + list(map(list, zip(*grid))):
         for i1, i2 in zip([0] + row, row + [0]):
