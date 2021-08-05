1f178aa8b74b74fa4caab7b783ade06fabce0308
IhebChatti <iheb.chatti@holbertonschool.com>
2021-08-05 21:43:59 +0100

    pascal's triangle

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -5,11 +5,11 @@
 from math import factorial
 
 
-def pascal_triangle(rows):
+def pascal_triangle(n):
     """[pascals_triangle]
     """
     result = []
-    for count in range(rows):
+    for count in range(n):
         row = []
         for element in range(count + 1):
             row.append(int((factorial(count)) / ((factorial(element)) * factorial(count - element))))
